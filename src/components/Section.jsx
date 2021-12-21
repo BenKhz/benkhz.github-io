48af45c72a8b711ab1760fee5b00a34493a82d47
Ben Bernardy <benbernardy@gmail.com>
2021-12-21 09:51:32 -0800

    ADDED: Animation Reset for <p> tags in Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function (props) {
   return (
-    <div id = {props.name} className="section text-shadow-drop-left">
-      <p className="appear">{props.content}</p>
+    <div id = {props.name} className="section">
+      <div className="appear" >
+        <p className="text-shadow-pop-bottom"
+          onMouseEnter={(e) => {
+            e.target.style.animation = "none" }}
+          onMouseLeave={(e) => {
+            e.target.style.animation = null
+          }}
+            >
+            {props.content}
+        </p>
+      </div>
     </div>
   );
 }
