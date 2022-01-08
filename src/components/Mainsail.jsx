8db465cf0e7eb4ff011a2587adea7ac03eab8f09
Ben Bernardy <benbernardy@gmail.com>
2022-01-08 14:04:44 -0800

    Minor tweak to typewriter

--- src/components/Mainsail.jsx
+++ src/components/Mainsail.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import Particles from "react-tsparticles";
 import TypeWriterEffect from 'react-typewriter-effect'
-
-const TypeWriterFill = (props) => {
-  return (
-    <span
-        className="emoji"
-        role="img"
-        aria-label={props.label ? props.label : ""}
-        aria-hidden={props.label ? "false" : "true"}
-    >
-        {`${props.text} ${props.symbol}`}
-    </span>
-  )
-}
 
 export default function (props) {
   const meDescripts = [
-    "Fullstack Dev!",
     "RESTful API builder!",
     "DBMS polyglot!",
-    "Bug Squasher!"
+    "Bug Squasher!",
+    "Pinball Wizard!",
+    "Fullstack Dev!",
   ]
   return (
     <div id={props.name} className="section">
