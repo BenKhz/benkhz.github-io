358f5ba7c4713e0e00188cfe27f6ba9bfcb7afea
Ben Bernardy <benbernardy@gmail.com>
2022-01-12 14:58:56 -0800

    Basic WebSockets up and running. Transferred to another repo, this branch no longer active

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const app = express();
 const PORT = 3000;
 const STATIC = path(__dirname, '/public')
 
+// import Ws express module
+const expressWs = require('express-ws')
+
+// adds Ws handlers to app instance
+expressWs(app);
+
+// normal static file server middleware
 app.use(express.static(STATIC));
+
+// normal route handler
+app.get('/hello', (req, res) => {
+  res.status(200).send("Successful GET to /hello")
+})
+// basic WS handler that should send msg.
+app.ws('/websocket', (ws, req) => {
+  ws.on('message', msg => {
+    console.log(msg)
+    ws.send(msg + " Recieved")
+  })
+})
 
 app.listen(PORT, (err) => { console.log(`Listening on ${PORT}`) })
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+
+const myWs = new WebSocket('ws://localhost:3000/websocket')
+myWs.addEventListener('message', (e) => {
+  console.log(e.data)
+  var myDiv = document.createElement("div")
+  myDiv.innerHTML = e.data
+  document.getElementById('socketsMsgs').appendChild(myDiv)
+})
 
 export default function () {
+  const [text, setText] = useState('');
+  const handleSend = () => {
+    myWs.send(text)
+  }
   return (
-    <div> React is running </div>
+    <>
+      <input placeholder="Text Here" value={text} onChange={(e) => {setText(e.target.value)}}></input>
+      <button onClick={handleSend}> Send WS Message! </button>
+      <div id='socketsMsgs'></div>
+    </>
   )
 }
