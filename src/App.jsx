e91139a60790ecea9cab805df58eb538a86a90ab
Ben Bernardy <benbernardy@gmail.com>
2022-01-16 12:28:47 -0800

    Start refactor into modular component

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,11 @@
 import React from "react";
-import ReactFullpage from '@fullpage/react-fullpage';
+import Clothesline from "./components/ClothesLine";
 
-import Mainsail from "./components/Mainsail";
-import Showoff from "./components/Showoff";
-import Contact from "./components/Contact";
-import SpeedDial from "./components/SpeedDial";
-
-import TwitterIcon from '@mui/icons-material/Twitter';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TagRoundedIcon from '@mui/icons-material/TagRounded';
+// Clothesline Custom Carousel component.
+//
 
 export default function () {
-  const pucks = [
-    {
-      name: "icon1",
-      icon: <GitHubIcon fontSize='large' />,
-      position: 0,
-      href: "https://github.com/BenKhz"
-    },
-    {
-      name: "icon2",
-      icon: <LinkedInIcon fontSize='large'/>,
-      position: 1,
-      href: "https://www.linkedin.com/in/benbernardy/"
-    },
-    {
-      name: "icon3",
-      icon: <TwitterIcon fontSize='large' />,
-      position: 2,
-      href: "https://twitter.com/benkhz"
-    },
-  ]
   return (
-    <>
-    <SpeedDial pucks={pucks} closeIcon={<TagRoundedIcon fontSize="large"/>}/>
-    <ReactFullpage
-      licenseKey={null}
-      sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
-      scrollingSpeed={1250}
-      navigation
-      render={() => {
-        return (
-          <ReactFullpage.Wrapper>
-            <Mainsail content="Hi, my name's Ben!" name="mainsail"/>
-            <Showoff content="My favorite tech!" name="showoff" />
-            <Contact content="Get in Touch!" name="contact"/>
-          </ReactFullpage.Wrapper>
-
-        );
-      }}
-      />
-      </>
+    <Clothesline />
   )
 }
--- src/components/ClothesLine.jsx
+++ src/components/ClothesLine.jsx
@@ -4,19 +4,11 @@ import ClothesLineItem from './ClothesLineItem';
 function Clothesline(props) {
 
   const [currCardInd, setCardInd] = useState(0)
-
-  const handlePrevClick = () => {
-    currCardInd === 0 ? setCardInd(props.projects.length-1) : setCardInd(prev => prev - 1);
-  }
-  const handleNextClick = () => {
-    currCardInd === props.projects.length-1 ? setCardInd(0) : setCardInd(prev => prev + 1);
-}
-  const cards = props.projects.map((project, ind) => <ClothesLineItem index={ind} text={project.shortText} currCardInd={currCardInd} image={project.thumbUrl} />)
   return (
     <div className="clContainer" >
       <svg
         style={{ position: 'relative', top: '0%', left: '0%', zIndex:'5' }}
-        onClick={handlePrevClick}
+        // onClick={handlePrevClick}
         height={'25vh'} width={'25vw'}
         viewBox="0 -3 10 10">
         <path
@@ -28,14 +20,14 @@ function Clothesline(props) {
       </svg>
       <div style={{postion:'relative'}}>
         <div style={{display:'flex', justifyContent:'center'}}>
-      {cards}
+      {/* {cards} */}
       </div>
       </div>
       <svg
         style={{ position: 'relative', top: '0', right: '0', zIndex:'5'}}
-        onClick={handleNextClick}
+        // onClick={handleNextClick}
         height={'25vh'} width={'25vw'}
-        viewBox="0 2 10 10" >
+        viewBox="-1 2 10 10" >
         <path d="m 10 3 l 0 3 l -3 0 l 3 0 q -4 -5 -7 -1"
           transform="rotate(30)"
           stroke="#1c8aff7a" strokeWidth="1"
